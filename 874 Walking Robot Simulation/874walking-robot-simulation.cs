7f3647a4a6f73bc0dd993580db0408f7c036bb53
Fabio Brea <fabio.brea@gmail.com>
2024-09-04 10:27:11 +0000

    Code challenge submission for question: 874 Walking Robot Simulation

---
+++ 874 Walking Robot Simulation/874walking-robot-simulation.cs
@@ -0,0 +1,50 @@
+public class Solution
+{
+    public int RobotSim(int[] commands, int[][] obstacles)
+    {
+        int[,] directions = new int[,] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+        
+        var obstacleMap = new Dictionary<(int, int), bool>();
+        foreach (var obstacle in obstacles)
+        {
+            obstacleMap[(obstacle[0], obstacle[1])] = true;
+        }
+
+        int x = 0, y = 0; 
+        int directionIndex = 0;
+        int maxDistance = 0;
+
+        foreach (var command in commands)
+        {
+            if (command == -1)
+            {
+                directionIndex = (directionIndex + 1) % 4;
+            }
+            else if (command == -2)
+            {
+                directionIndex = (directionIndex + 3) % 4;
+            }
+            else
+            {
+                for (int i = 0; i < command; i++)
+                {
+                    int nextX = x + directions[directionIndex, 0];
+                    int nextY = y + directions[directionIndex, 1];
+
+                    if (!obstacleMap.ContainsKey((nextX, nextY)))
+                    {
+                        x = nextX;
+                        y = nextY;
+                        maxDistance = Math.Max(maxDistance, x * x + y * y);
+                    }
+                    else
+                    {
+                        break;  // Stop moving in this direction if an obstacle is encountered
+                    }
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+}
